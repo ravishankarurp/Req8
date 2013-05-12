43f2069317568ebed0fb45f1dba7f59d16f6be0d
Ravi Parupudi <ravishankarurp@gmail.com>
2013-05-12 21:57:58 +0100

    First changes

--- client/default/app/view/Main.js
+++ client/default/app/view/Main.js
@@ -23,7 +23,7 @@ Ext.define('Req8.view.Main', {
                 },
 
                 html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
+                    "Ravi, You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
                     "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
                     "and refresh to change what's rendered here."
                 ].join("")
